0faea56d17c48dcc87593be7d48aca4fcd32bf78
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-22 17:24:45 +0300

    feat: Data transfer with navigation and api request at useFetch are now work properly

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,20 +3,16 @@ import axios from "axios"
 import { Alert } from "react-native";
 
 
-const useFetch = () => {
+const useFetch = (id) => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(true)
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=c10efc3a23bb8482602e195f69924f93&hash=d051dfd0bf99bcc7d3aa2cb83ca5dd1d`)
+      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters${id}?ts=1&apikey=c10efc3a23bb8482602e195f69924f93&hash=d051dfd0bf99bcc7d3aa2cb83ca5dd1d`)
       setData(response.data.data.results)
-      
-      console.log(response.data.data.results)
-
     } catch (error) {
       Alert.alert(error.message)
-
     } finally {
       setLoading(false)
     }
--- src/pages/Detail/detail.js
+++ src/pages/Detail/detail.js
@@ -1,14 +1,25 @@
 import { useRoute } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, Text} from 'react-native';
+import { Image, SafeAreaView, Text} from 'react-native';
+import useFetch from '../../hooks/useFetch';
 import styles from './detail.styles';
 
-export default function() {
-    const routes = useRoute()
-    const {data} = routes.params
+export default function Detail () {
+    const route = useRoute()
+    const id = route.params.item.id
+
+    const {data, loading} = useFetch(`/${id}`)
+
+    const source = {...data[0]}
+
+    console.log(data)
     return (
         <SafeAreaView>
-            <Text>{data.name}</Text>
+            <Image 
+                style={styles.image}
+                source={{uri:`${source.thumbnail.path}.jpg`}}/>
+            <Text>{source.name}</Text>
+            <Text>{source.description}</Text>
         </SafeAreaView>
     )
 }
