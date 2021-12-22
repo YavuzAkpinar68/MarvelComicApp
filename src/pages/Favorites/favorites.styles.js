11dfcc2cce3cef81f70a2ba1768bd63b03f7b832
Selahattin Demir <selahattindemir686@gmail.com>
2021-12-22 20:03:31 +0300

    Favorites page has been created.

--- src/pages/Favorites/favorites.styles.js
+++ src/pages/Favorites/favorites.styles.js
@@ -1,5 +1,29 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 
-export default StyleSheet.create({
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginLeft:10,
+    marginRight:10,
+    marginBottom:10,
+  },
+  image:{
+    flex:1,
+    height:Dimensions.get("screen").height/2,
+    width:Dimensions.get("window").width/1,
+    justifyContent:"flex-end",
 
-});
+  },
+  name:{
+    fontSize:25,
+    color:"white"
+  },
+  textView:{
+    justifyContent:"space-around",
+    backgroundColor:"#000000a0"
+  },
+  comicsView:{
+  }
+})
+
+export default styles
