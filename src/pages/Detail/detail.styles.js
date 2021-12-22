286c23945144ef805063bc47921abddf007ffe20
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-22 17:25:04 +0300

    style:Style changes

--- src/pages/Detail/detail.styles.js
+++ src/pages/Detail/detail.styles.js
@@ -1,5 +1,8 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    
+    image:{
+      height:Dimensions.get("screen").height/2,
+      resizeMode:"contain"
+    }
 })
