04d346860b63474776d517bb297773ee44abbd8e
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-23 20:04:53 +0300

    style Style change

--- src/Component/Cards/MainPageCardsStyle.js
+++ src/Component/Cards/MainPageCardsStyle.js
@@ -3,14 +3,12 @@ import { Dimensions, StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    marginLeft:10,
-    marginRight:10,
-    marginBottom:10,
+    margin:10,
   },
   image:{
     flex:1,
-    height:Dimensions.get("screen").height/2,
-    width:Dimensions.get("window").width/1,
+    height:Dimensions.get("screen").height/4,
+    width:Dimensions.get("window").width/2.3,
     justifyContent:"flex-end",
 
   },
