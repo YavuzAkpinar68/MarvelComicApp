8a75d707e9acfb827339836122a3a7f06db38021
Selahattin Demir <selahattindemir686@gmail.com>
2021-12-23 18:18:33 +0300

    Adding and deleting to favorites has been done.

--- src/pages/Detail/detail.js
+++ src/pages/Detail/detail.js
@@ -1,56 +1,74 @@
-import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Image, SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect, useState, useContext} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import Button from '../../Component/Button/button';
 import useFetch from '../../hooks/useFetch';
 import styles from './detail.styles';
 import ComicCard from '../../Component/Cards/ComicCard/ComicCard';
+import {MarvelContext} from '../../Context/MarvelProvider';
 
-export default function Detail () {
-    const route = useRoute()
-    const item = route.params.item
-    const navigation = useNavigation()
-    const [product, setProduct] = useState("comics")
-    
-    const handleProduct = (a) => {
-        setProduct(a)
-    }
-    const handleCloseProduct = () => {
-        setProduct("")
-    }
-    const handleNavigation = () => {
-        navigation.navigate("ComicPage")
-    }
-    
-    const endpoint = product
-    const {data, loading} = useFetch(`/${item.id}/${endpoint}`)
-    console.log(product)
-    return (
-        <SafeAreaView style={{flex:1}}>
-            <View style={styles.cardContainer}>
-                <Image 
-                    style={styles.image}
-                    source={{uri:`${item.thumbnail.path}.jpg`}}/>
-                <View style={styles.textView}>
-                    <Text style={styles.name}>{item.name}</Text>
-                    {
-                        item.description ?
-                        <Text style={styles.description}>{item.description}</Text>
-                        :<Text style={styles.description}>No description</Text>
-                    }
-                </View>
-            </View>
-            <View style={styles.buttonView}>
-                <Button title="comics" onPress={handleProduct}/>
-                <Button title="close comics" onPress={handleCloseProduct}/>
-            </View>
-            {
-                loading ? <ActivityIndicator />:
-                <FlatList
-                    data={product && data}
-                    renderItem={({item}) => <ComicCard onPress={handleNavigation} item={item}/>}
-                />
-            }
-        </SafeAreaView>
-    )
+export default function Detail() {
+  const route = useRoute();
+  const item = route.params.item;
+  const navigation = useNavigation();
+  const [product, setProduct] = useState('comics');
+
+  const {dispatch} = useContext(MarvelContext);
+
+  const handleProduct = a => {
+    setProduct(a);
+  };
+  const handleCloseProduct = () => {
+    setProduct('');
+  };
+  const handleNavigation = () => {
+    navigation.navigate('ComicPage');
+  };
+  const handleAddFavorites = marvel =>
+    dispatch({type: 'ADD_TO_FAVORITES', payload: {marvel}});
+
+  const endpoint = product;
+  const {data, loading} = useFetch(`/${item.id}/${endpoint}`);
+  
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.cardContainer}>
+        <Image
+          style={styles.image}
+          source={{uri: `${item.thumbnail.path}.jpg`}}
+        />
+        <View style={styles.textView}>
+          <Text style={styles.name}>{item.name}</Text>
+          {item.description ? (
+            <Text style={styles.description}>{item.description}</Text>
+          ) : (
+            <Text style={styles.description}>No description</Text>
+          )}
+        </View>
+      </View>
+      <View style={styles.buttonView}>
+        <Button title="comics" onPress={handleProduct} />
+        <Button title="close comics" onPress={handleCloseProduct} />
+      </View>
+      <Button title="Add Favorites" onPress={() => handleAddFavorites(item)} />
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={product && data}
+          renderItem={({item}) => (
+            <ComicCard onPress={handleNavigation} item={item} />
+          )}
+        />
+      )}
+    </SafeAreaView>
+  );
 }
--- src/pages/Favorites/favorites.js
+++ src/pages/Favorites/favorites.js
@@ -1,45 +1,22 @@
 import React, {useContext} from 'react';
-import {
-  SafeAreaView,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  ImageBackground,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text, FlatList} from 'react-native';
 import {MarvelContext} from '../../Context/MarvelProvider';
 import styles from './favorites.styles';
+import MainPageCard from '../../Component/Cards/MainPageCard';
 
 export default function Favorites() {
-  const [state, dispatch] = useContext(MarvelContext);
+  const {state, dispatch} = useContext(MarvelContext);
 
   const handleRemoveFavorites = marvel =>
     dispatch({type: 'REMOVE_FROM_FAVORITES', payload: {marvel}});
-  const renderMarvel = ({item}) => {
-    return (
-      <TouchableOpacity
-        onPress={() => handleRemoveFavorites(item)}
-        style={styles.container}>
-        <ImageBackground
-          source={{uri: `${item.thumbnail.path}.jpg`}}
-          style={styles.image}>
-          <View style={styles.textView}>
-            <Text style={styles.name}>{item.name}</Text>
-            <View style={styles.comicsView}>
-              <Text>Available Comics : {item.comics.available}</Text>
-              <Text>Available Stories : {item.stories.available}</Text>
-              <Text>Available Series : {item.series.available}</Text>
-            </View>
-          </View>
-        </ImageBackground>
-      </TouchableOpacity>
-    );
-  };
+
+  const renderMarvel = ({item}) => (
+    <MainPageCard item={item} onPress={() => handleRemoveFavorites(item)} />
+  );
 
   return (
     <SafeAreaView>
       <Text>Favorites</Text>
-
       <FlatList data={state.favoritesList} renderItem={renderMarvel} />
     </SafeAreaView>
   );
