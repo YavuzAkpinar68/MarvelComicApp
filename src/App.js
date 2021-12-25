91203a04af3eb828ef905f52c982e352da698462
Selahattin Demir <selahattindemir686@gmail.com>
2021-12-25 21:41:57 +0300

    feat: Add theme context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Navigation from './Navigation';
 import MarvelProvider from './Context/MarvelProvider';
 import TranslationProvider from './Context/TranslationContext/TranslationProvider';
+import ThemeProvider from './Context/ThemeContext/ThemeContext';
 
 export default function App() {
   return (
-    <TranslationProvider>
-      <MarvelProvider>
-        <Navigation />
-      </MarvelProvider>
-    </TranslationProvider>
+    <ThemeProvider>
+      <TranslationProvider>
+        <MarvelProvider>
+          <Navigation />
+        </MarvelProvider>
+      </TranslationProvider>
+    </ThemeProvider>
   );
 }
--- src/pages/SettingsPage/setting.js
+++ src/pages/SettingsPage/setting.js
@@ -1,38 +1,49 @@
-import React, { useContext, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Button, SafeAreaView, Switch, SwitchComponent, Text } from "react-native";
-import SwitchSelector from "react-native-switch-selector";
-import { TranslationContext } from "../../Context/TranslationContext/TranslationProvider";
+import React, {useContext, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import {
+  Button,
+  SafeAreaView,
+  Switch,
+  SwitchComponent,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import SwitchSelector from 'react-native-switch-selector';
+import {ThemeContext} from '../../Context/ThemeContext/ThemeContext';
+import {TranslationContext} from '../../Context/TranslationContext/TranslationProvider';
 import styles from './settings.styles';
 
 const Settings = () => {
-    const {t, i18n} = useTranslation()
-    const {language, setLanguage} = useContext(TranslationContext)
-    const [value, setValue] = useState("false")
-    const handleEnglishLanguage = () => {
-        setLanguage("en")
-        setValue(false)
-        i18n.changeLanguage(language)
-    }
-    const handleTurkishLanguage = () => {
-        setLanguage("tr")
-        setValue(true)
-        i18n.changeLanguage(language)
-    }
-    const options = [
-        {label:"English", value:"en"},
-        {label:"Turkish", value:"tr"}
+  const {t, i18n} = useTranslation();
+  const {language, setLanguage} = useContext(TranslationContext);
+  const [value, setValue] = useState('false');
+  const handleEnglishLanguage = () => {
+    setLanguage('en');
+    setValue(false);
+    i18n.changeLanguage(language);
+  };
+  const handleTurkishLanguage = () => {
+    setLanguage('tr');
+    setValue(true);
+    i18n.changeLanguage(language);
+  };
+  const options = [
+    {label: 'English', value: 'en'},
+    {label: 'Turkish', value: 'tr'},
+  ];
+  const {theme, setTheme} = useContext(ThemeContext);
 
-    ]
-    return(
-        <SafeAreaView>
-            <SwitchSelector
-                options={options}
-                selectedColor="red"
-                hasPadding
-                onPress={(language) => i18n.changeLanguage(language) }></SwitchSelector>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView>
+      <SwitchSelector
+        options={options}
+        selectedColor="red"
+        hasPadding
+        onPress={language => i18n.changeLanguage(language)}></SwitchSelector>
+      <Button title="dark" onPress={() => setTheme()} />
+      
+    </SafeAreaView>
+  );
+};
 
-export default Settings
+export default Settings;
