a052cbeb2ab3d61e0f598632209dceb31a39b630
yavuz <yavuz.akpinar.68@gmail.com>
2021-12-27 22:58:36 +0300

    feat: Async structure added to the theme

--- src/pages/SettingsPage/setting.js
+++ src/pages/SettingsPage/setting.js
@@ -1,12 +1,14 @@
 import React, {useContext, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {
-  Button,
   SafeAreaView,
+  View,
+  Text
 } from 'react-native';
 import SwitchSelector from 'react-native-switch-selector';
 import {ThemeContext} from '../../Context/ThemeContext/ThemeProvider';
 import styles from './settings.styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Settings = () => {
   const {t, i18n} = useTranslation();
@@ -16,16 +18,43 @@ const Settings = () => {
     {label: 'English', value: 'en'},
     {label: 'Turkish', value: 'tr'},
   ];
+  const themeOptions = [
+    {label: 'Dark', value: 'dark'},
+    {label: 'Default', value: 'default'},
+  ];
+
+  const handleTheme = value => {
+    setTheme(value);
+    AsyncStorage.setItem('@THEME', value);
+  };
   
     return(
         <SafeAreaView>
+           <View style={styles.languages}>
+            <Text style={styles.languages_text}>{t('Languages')} </Text>
             <SwitchSelector
-                options={options}
-                selectedColor="red"
-                hasPadding
-                onPress={(language) => i18n.changeLanguage(language) }></SwitchSelector>
-                <Button title="dark" onPress={() => setTheme("dark")}></Button>
-                <Button title="default" onPress={() => setTheme("default")}></Button>
+            options={options}
+            selectedColor="white"
+            buttonColor="teal"
+            bold
+            textStyle={{fontSize: 20}}
+            selectedTextStyle={{fontSize: 20}}
+            hasPadding
+            onPress={language => i18n.changeLanguage(language)}></SwitchSelector>
+        </View>
+      <View style={styles.theme}>
+        <Text style={styles.theme_text}>{t('Theme')} </Text>
+        <SwitchSelector
+          options={themeOptions}
+          selectedColor="white"
+          buttonColor="teal"
+          bold
+          textStyle={{fontSize: 20}}
+          selectedTextStyle={{fontSize: 20}}
+          hasPadding
+          onPress={handleTheme}></SwitchSelector>
+      </View>
+
         </SafeAreaView>
     )
 }
--- src/pages/SettingsPage/settings.styles.js
+++ src/pages/SettingsPage/settings.styles.js
@@ -1,5 +1,22 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    
+  languages: {
+    margin: 5,
+    marginTop: 25,
+  },
+  languages_text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  theme_text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+
+  theme: {
+    margin: 5,
+    marginTop: 15,
+  },
+
 })
